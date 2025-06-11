a883c0774682c42ea5452dbc278abc0ce5828b59
FGP <elaifigueroa@gmail.com>
2025-06-11 16:47:20 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,3 @@
+
+        console.log("Este Site Foi feita por um aluno de Marly, ele é o Elai , programador, o conteúdo da história é de Marly. Agradecemos a visita!");
+        console.log("Copyright © 2025 todos os direitos reservados por FREE GAME PLANT (FGP)");
